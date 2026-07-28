9ad34d887971aa94924f661c4bd36e00814cf98a
Cse15-0737 <aryanbrijrajrai@gmail.com>
2026-07-28 11:26:32 +0530

    JS Basics Revision

---
+++ JS/index.js
@@ -0,0 +1,13 @@
+console.log("Hello, World!");
+console.log("3+6 = " +(3+6));
+console.log("This is a simple JavaScript Program");
+console.log("Goodbye!");
+console.error("This is an error message");
+console.warn("This is a warning message");
+console.info("This is an informational message");
+console.debug("This is a debug message");
+console.log(process.platform); // Outputs the platform the code is running on (e.g., 'win32', 'darwin', 'linux')
+console.log(global.Lnumber);
+global.Lnumber = 2; // Assigns the value 2 to the global variable Lnumber.
+console.log(global.Lnumber);
+console.log(typeof(global.Lnumber));
