2fe228f47f110e4e4b86d5cc8a2546e27a3b56ea
Cse15-0737 <aryanbrijrajrai@gmail.com>
2026-07-28 12:11:40 +0530

    JSON Package intialization

--- JS/index.js
+++ JS/index.js
@@ -1,13 +1,25 @@
-console.log("Hello, World!");
-console.log("3+6 = " +(3+6));
-console.log("This is a simple JavaScript Program");
-console.log("Goodbye!");
-console.error("This is an error message");
-console.warn("This is a warning message");
-console.info("This is an informational message");
-console.debug("This is a debug message");
-console.log(process.platform); // Outputs the platform the code is running on (e.g., 'win32', 'darwin', 'linux')
-console.log(global.Lnumber);
-global.Lnumber = 2; // Assigns the value 2 to the global variable Lnumber.
-console.log(global.Lnumber);
-console.log(typeof(global.Lnumber));
+// console.log("Hello, World!");
+// console.log("3+6 = " +(3+6));
+// console.log("This is a simple JavaScript Program");
+// console.log("Goodbye!");
+// console.error("This is an error message");
+// console.warn("This is a warning message");
+// console.info("This is an informational message");
+// console.debug("This is a debug message");
+// console.log(process.platform); // Outputs the platform the code is running on (e.g., 'win32', 'darwin', 'linux')
+// console.log(global.Lnumber);
+// global.Lnumber = 2; // Assigns the value 2 to the global variable Lnumber.
+// console.log(global.Lnumber);
+// console.log(typeof(global.Lnumber));
+
+// Events
+// process.on("exit", function () {
+//   console.log("good");
+// });
+
+const { EventEmitter } = require("events");
+const eventEmitter = new EventEmitter();
+eventEmitter.on("lunch", () => {
+  console.log("good");
+});
+eventEmitter.emit("lunch");
